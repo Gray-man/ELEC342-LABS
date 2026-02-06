0a20ccc2b46f11b96de61b0008d53a93e6ffa3a2
Lucas Graham <lu_graha@major.encs.concordia.ca>
2026-02-06 14:54:56 -0500

    add mor estuff

--- P2Q3.m
+++ P2Q3.m
@@ -1,11 +1,27 @@
-x1 = [-10:0];
-x2 = [0:10];
+x1 = [-10:10];
+x2 = [20:40];
 
+
+% Linearity test
 y1 = Sys1(x1 + x2);
 y2 = Sys1(x1) + Sys1(x2);
 
-if y1 == y2
-    disp("Sys1 is linear\n");
+if (y1 == y2)
+    disp("Sys1 is linear");
 else
-    disp("Sys1 is not linear\n");
+    disp("Sys1 is not linear");
 end;
+
+% Time invariance test
+y3 = Sys1(x1);
+y4 = Sys1(x2);
+
+figure(1);
+subplot(1,2,1); stem(x1,y3);
+subplot(1,2,2); stem(x2,y4);
+
+% if (y3 == y4)
+%     disp("Sys1 is time invariant");
+% else
+%     disp("Sys1 is not time invariant");
+% end;
